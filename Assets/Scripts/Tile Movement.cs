e79d8f00eed5f04fcf736c3a5fcaa06995fe000a
kwanyinlai <laikwanyin06@gmail.com>
2024-10-26 18:06:24 -0400

    Map Generation added.

--- Assets/Scripts/Tile Movement.cs
+++ Assets/Scripts/Tile Movement.cs
@@ -3,26 +3,33 @@ using System;
 
 public class Isometric2DMovement : MonoBehaviour
 {
+    [SerializeField] private int x_pos;
+    [SerializeField] private int y_pos;
+    void Start(){
+        transform.position=new Vector3(0f,0f,0f);
+    }
 
     void Update()
     {  
-        Vector3 pos =transform.position;
+
+
+        Vector3 pos = transform.position;
         if (Input.GetKeyDown("w")){
-             pos = new Vector3((float)(transform.position.x + 0.5), (float)(transform.position.y+0.25), 0f);
+            y_pos+=1;
         }
         else if (Input.GetKeyDown("s")){
-             pos = new Vector3((float)(transform.position.x - 0.5), (float)(transform.position.y - 0.25), 0f);
+             y_pos-=1;
         }
         else if (Input.GetKeyDown("a")){
-             pos = new Vector3((float)(transform.position.x - 0.5), (float)(transform.position.y + 0.25), 0f);
+             x_pos-=1;
         }
         else if (Input.GetKeyDown("d")){
-             pos = new Vector3((float)(transform.position.x + 0.5), (float)(transform.position.y - 0.25), 0f);
-        
+            x_pos+=1;
         }
         
+
         
-        transform.position=pos;
+        transform.position = new Vector3( (float)(y_pos * 0.5 + x_pos * 0.5), (float)(y_pos * 0.25 - x_pos *0.25),0f);
 
     }
 }
