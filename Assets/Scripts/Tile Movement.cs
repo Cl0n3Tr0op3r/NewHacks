e124c2eb5a322c1a1fa8185b13fb3451ec7e7cd9
kwanyinlai <laikwanyin06@gmail.com>
2024-10-26 17:25:22 -0400

    Fixed movement.

--- Assets/Scripts/Tile Movement.cs
+++ Assets/Scripts/Tile Movement.cs
@@ -1,40 +1,28 @@
 using UnityEngine;
+using System;
 
 public class Isometric2DMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
 
     void Update()
-    {
-        MoveCharacter();
-    }
-
-    void MoveCharacter()
-    {
-      
-        float horizontal = Input.GetAxis("Horizontal") * -1; 
-        float vertical = Input.GetAxis("Vertical");     
-
-
-        Vector2 moveDirection = Vector2.zero;
-
-       
-        if (horizontal != 0 || vertical != 0)
-        {
-       
-            moveDirection = new Vector2(vertical, horizontal);
-            
-            
-            Vector2 isometricDirection = new Vector2(
-                moveDirection.x - moveDirection.y, 
-                (moveDirection.x + moveDirection.y) * 0.5f
-            );
-
-           
-            isometricDirection.Normalize();
-
-         
-            transform.position += (Vector3)isometricDirection * moveSpeed * Time.deltaTime;
+    {  
+        Vector3 pos =transform.position;
+        if (Input.GetKeyDown("w")){
+             pos = new Vector3((float)(transform.position.x + 0.5), (float)(transform.position.y+0.25), 0f);
         }
+        else if (Input.GetKeyDown("s")){
+             pos = new Vector3((float)(transform.position.x - 0.5), (float)(transform.position.y - 0.25), 0f);
+        }
+        else if (Input.GetKeyDown("a")){
+             pos = new Vector3((float)(transform.position.x - 0.5), (float)(transform.position.y + 0.25), 0f);
+        }
+        else if (Input.GetKeyDown("d")){
+             pos = new Vector3((float)(transform.position.x + 0.5), (float)(transform.position.y - 0.25), 0f);
+        
+        }
+        
+        
+        transform.position=pos;
+
     }
 }
