86bcf5ec329efadb04f36f27e2ae87ad0a3b8e8f
C20H12 <marvinm2@educbe.ca>
2024-10-26 20:02:54 -0400

    delete old movement file

--- Assets/Scripts/Tile Movement.cs
+++ Assets/Scripts/Tile Movement.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Isometric2DMovement : MonoBehaviour
 {
@@ -10,7 +12,9 @@ public class Isometric2DMovement : MonoBehaviour
     TilemapMapGenerator map;
 
     [SerializeField] public bool isTimePaused = false;
-    public static LinkedList<GameObject> players = new LinkedList<>();
+    public Queue<int> player_inputs = new Queue<int>();
+
+    public static LinkedList<GameObject> players = new LinkedList<GameObject>();
 
 
     void Start()
@@ -18,13 +22,34 @@ public class Isometric2DMovement : MonoBehaviour
         transform.position=new Vector3(0f,0f,0f);
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         map = GameObject.Find("Grid").GetComponent<TilemapMapGenerator>();
-        players.AddLast(this);
+        // players.AddLast(this);
     }
 
     void Update()
     {  
-        
-
+        if (!isTimePaused)
+        {
+            if (player_inputs.Count != 0) {
+                foreach (var dir in player_inputs)
+                {
+                    move(dir);
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown("w")) move(1);
+                if (Input.GetKeyDown("a")) move(2);
+                if (Input.GetKeyDown("s")) move(3);
+                if (Input.GetKeyDown("d")) move(4);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown("w")) player_inputs.Enqueue(1);
+            if (Input.GetKeyDown("a")) player_inputs.Enqueue(2);
+            if (Input.GetKeyDown("s")) player_inputs.Enqueue(3);
+            if (Input.GetKeyDown("d")) player_inputs.Enqueue(4);
+        }
     }
 
     void move(int dir) {
@@ -33,25 +58,25 @@ public class Isometric2DMovement : MonoBehaviour
         
         Vector3 pos = transform.position;
         
-        if (1){
+        if (dir == 1){
             if((map.end_y)>=y_pos){
                 y_pos+=1;
             }
             spriteRenderer.sprite = spriteArray[1];
         }
-        else if (2){
+        else if (dir == 2){
             if(map.start_y+2<y_pos){
                 y_pos-=1;
             }
              spriteRenderer.sprite = spriteArray[3];
         }
-        else if (3){
+        else if (dir == 3){
             if(map.start_x<=x_pos){
                 x_pos-=1;
             }
              spriteRenderer.sprite = spriteArray[2];
         }
-        else if (4){
+        else if (dir == 4){
             if((-2+map.end_x)>x_pos){
                 x_pos+=1;
             }
